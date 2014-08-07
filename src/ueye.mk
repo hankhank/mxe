9a282863ef0b80ad6f53b6533dff9af6a8a74bac
Andrew Hankins <andrew@sideramota.com>
2014-08-07 21:02:44 +1000

    Adding required dll to package

--- src/ueye.mk
+++ src/ueye.mk
@@ -4,7 +4,7 @@
 PKG             := ueye
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 4.40.1_32_WHQL
-$(PKG)_CHECKSUM := 64a104f24a383a5b1d307c02aac67dbc656c1599
+$(PKG)_CHECKSUM := 288a9e6dc37345a2d4794baee342f10c0617f160
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).zip
 $(PKG)_URL      := 
@@ -12,5 +12,6 @@ $(PKG)_DEPS     :=
 
 define $(PKG)_BUILD
     cd $(1) && $(INSTALL) include/* $(PREFIX)/$(TARGET)/include && \
-	$(INSTALL) lib/* $(PREFIX)/$(TARGET)/lib
+	$(INSTALL) lib/* $(PREFIX)/$(TARGET)/lib && \
+	$(INSTALL) bin/* $(PREFIX)/$(TARGET)/bin
 endef
